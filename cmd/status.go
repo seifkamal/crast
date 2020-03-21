06dbd3225bfd5f7893a19d5dd32776abf17c5842
Seif Kamal <seif@tickx.co.uk>
2020-03-21 19:53:00 +0000

    Add status interactive selection mode

--- cmd/status.go
+++ cmd/status.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
@@ -15,9 +16,34 @@ func init() {
 		Version: "1.0.0",
 		Args:    cobra.ExactArgs(0),
 		Run: func(cmd *cobra.Command, args []string) {
-			list, listDir := locker.Lists.Get(dir)
+			if len(locker.Lists) == 0 {
+				cmd.Println("No lists initialised")
+				return
+			}
+
+			interactive, err := cmd.Flags().GetBool("interactive")
+			if err != nil {
+				cmd.PrintErrln(err)
+				return
+			}
+
+			selectedDir := dir
+			if interactive {
+				prompt := promptui.Select{
+					Label: "Select directory list to show status for",
+					Items: locker.Dirs(),
+				}
+
+				_, selectedDir, err = prompt.Run()
+				if err != nil {
+					cmd.PrintErrln(err)
+					return
+				}
+			}
+
+			list, listDir := locker.Lists.Get(selectedDir)
 			if len(*list) == 0 {
-				cmd.Println("List", dir, "is empty")
+				cmd.Println("List", selectedDir, "is empty")
 				return
 			}
 
@@ -36,6 +62,7 @@ func init() {
 
 	cmd.Flags().StringP("topic", "t", "", "Filter tasks by topic")
 	cmd.Flags().BoolP("all", "a", false, "Show all tasks (including ones marked as done)")
+	cmd.Flags().BoolP("interactive", "i", false, "Interactively select which list to show status for")
 
 	mainCmd.AddCommand(cmd)
 }
